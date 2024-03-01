f870902bef326b7e7f69ee17c45bf41e53a6b6b0
pmammino <paul.mammino@wagner.edu>
2024-03-01 12:20:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def get_template(type_id):
     results['prediction'] = ""
     template = results[results.type_custom_types == type_id]
     template = template[
-        ["amount_number", "player_id_text", "player_text", "type_custom_types", "Date","prediction"]]
+        ["_id","amount_number", "player_id_text", "player_text", "Date","prediction"]]
     return template.to_json(orient='records')
 
 
