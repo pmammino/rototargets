2b5f318939a6bc1b6d325a45818e157605d5f25a
pmammino <paul.mammino@wagner.edu>
2024-03-16 14:58:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def get_template(type_id):
     else:
         return print("Could not connect")
     results = pd.DataFrame(list(rows), columns=["id", "amount", "player", "player_id", "type"])
-    results['Date'] = str(datetime.date.today())
+    results['date'] = str(datetime.date.today())
     results['prediction'] = ""
     template = results[results.type == name]
     if name == "MLB - Strikeouts":
