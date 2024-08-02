8e4f59caf3149bc838ee943c8cacd2620b2399ac
pmammino <paul.mammino@wagner.edu>
2024-08-02 19:17:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1204,65 +1204,219 @@ def bet_finder_strikeouts(post_id):
     predictions_live = predictions_live.sort_values(["diff"], ascending=[False])
     return predictions_live.to_json(orient='records')
 
-@application.route("/test_bet_finder")
-def test_bet():
-    # API endpoint and key
-    url = "https://api.the-odds-api.com/v4/sports/baseball_mlb/events/"
-    api_key = "22a6282c9744177b06acb842d34a02cb"
-    params = {
-        'apiKey': api_key,
-        'regions': 'us',
-        ##'markets': 'h2h,spreads',
-        ##'markets': 'pitcher_strikeouts_alternate,batter_total_bases',
-        'markets': 'pitcher_strikeouts_alternate',
-        'oddsFormat': 'american',
-        'commenceTimeFrom': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-        'commenceTimeTo': (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime(
-            '%Y-%m-%dT%H:%M:%SZ')
-    }
-    response = requests.get(url, params=params)
-    data = response.json()
-
-    events = []
-
-    for event in data:
-        events.append(event['id'])
-
-    flattened_markets = []
-
-    for event in events:
-        url = "https://api.the-odds-api.com/v4/sports/baseball_mlb/events/" + event + "/odds"
+@application.route("/test_bet_finder/<string:market>")
+@application.route("/test_bet_finder/<string:market>/<string:books>")
+def test_bet(market,books = None):
+    if market == "MLB - Strikeouts":
+        # API endpoint and key
+        url = "https://api.the-odds-api.com/v4/sports/baseball_mlb/events/"
         api_key = "22a6282c9744177b06acb842d34a02cb"
-        params = {
-            'apiKey': api_key,
-            'regions': 'us',
-            ##'markets': 'h2h,spreads',
-            'markets': 'pitcher_strikeouts,pitcher_strikeouts_alternate',
-            'oddsFormat': 'american',
-            'bookmakers': 'draftkings,fanduel,fliff',
-            'commenceTimeFrom': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
-            'commenceTimeTo': (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime(
-                '%Y-%m-%dT%H:%M:%SZ')
-        }
-        # x = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+        if books is not None:
+            params = {
+                'apiKey': api_key,
+                'regions': 'us',
+                ##'markets': 'h2h,spreads',
+                ##'markets': 'pitcher_strikeouts_alternate,batter_total_bases',
+                'bookmakers': books,
+                'markets': 'pitcher_strikeouts_alternate',
+                'oddsFormat': 'american',
+                'commenceTimeFrom': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'commenceTimeTo': (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime(
+                    '%Y-%m-%dT%H:%M:%SZ')
+            }
+        else:
+            params = {
+                'apiKey': api_key,
+                'regions': 'us',
+                ##'markets': 'h2h,spreads',
+                ##'markets': 'pitcher_strikeouts_alternate,batter_total_bases',
+                'markets': 'pitcher_strikeouts_alternate',
+                'oddsFormat': 'american',
+                'commenceTimeFrom': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'commenceTimeTo': (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime(
+                    '%Y-%m-%dT%H:%M:%SZ')
+            }
+        response = requests.get(url, params=params)
+        data = response.json()
 
-        # x = (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime('%Y-%m-%dT%H:%M:%SZ')
+        events = []
+
+        for event in data:
+            events.append(event['id'])
+
+        flattened_markets = []
+
+        for event in events:
+            url = "https://api.the-odds-api.com/v4/sports/baseball_mlb/events/" + event + "/odds"
+            api_key = "22a6282c9744177b06acb842d34a02cb"
+            params = {
+                'apiKey': api_key,
+                'regions': 'us',
+                ##'markets': 'h2h,spreads',
+                'markets': 'pitcher_strikeouts,pitcher_strikeouts_alternate',
+                'oddsFormat': 'american',
+                'bookmakers': 'draftkings,fanduel,fliff',
+                'commenceTimeFrom': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'commenceTimeTo': (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime(
+                    '%Y-%m-%dT%H:%M:%SZ')
+            }
+            # x = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+            # x = (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime('%Y-%m-%dT%H:%M:%SZ')
+            # Fetch the data from the API
+            response = requests.get(url, params=params)
+            data = response.json()
+
+            # Extract and flatten markets data
+
+            for entry in data:
+                game_info = {
+                    "id": data["id"],
+                    "sport_key": data["sport_key"],
+                    "sport_title": data["sport_title"],
+                    "commence_time": data["commence_time"],
+                    "home_team": data["home_team"],
+                    "away_team": data["away_team"]
+                }
+                for bookmaker in data["bookmakers"]:
+                    bookmaker_info = {
+                        "bookmaker_key": bookmaker["key"],
+                        "bookmaker_title": bookmaker["title"]
+                    }
+                    for market in bookmaker["markets"]:
+                        market_info = game_info.copy()
+                        market_info.update(bookmaker_info)
+                        market_info.update({
+                            "market_key": market["key"],
+                        })
+                        for outcome in market["outcomes"]:
+                            outcome_info = market_info.copy()
+                            if "point" in outcome:
+                                outcome_info.update({
+                                    "player_name": outcome["description"],
+                                    "outcome_name": outcome["name"],
+                                    "outcome_price": outcome["price"],
+                                    "outcome_point": outcome["point"]
+                                })
+                            else:
+                                outcome_info.update({
+                                    "player_name": outcome["description"],
+                                    "outcome_name": outcome["name"],
+                                    "outcome_price": outcome["price"],
+                                    "outcome_point": 0
+                                })
+                            flattened_markets.append(outcome_info)
+
+        # Convert flattened markets data into a DataFrame
+        df = pd.DataFrame(flattened_markets)
+        max_indices = df.groupby(['outcome_name', 'outcome_point', 'player_name'])['outcome_price'].idxmax()
+        filtered_df = df.loc[max_indices]
+        filtered_df['Im_Prob'] = np.where(filtered_df['outcome_price'] >= 0, 100 / (100 + filtered_df['outcome_price']),
+                                          -filtered_df['outcome_price'] / (-filtered_df['outcome_price'] + 100))
+
+        filtered_df = filtered_df[filtered_df['outcome_name'] == 'Over']
+
+        cnx = mysql.connector.connect(user='doadmin', password='AVNS_Lkaktbc2QgJkv-oDi60',
+                                      host='db-mysql-nyc3-89566-do-user-8045222-0.c.db.ondigitalocean.com',
+                                      port=25060,
+                                      database='crowdicate')
+        if cnx and cnx.is_connected():
+            with cnx.cursor() as cursor:
+                result = cursor.execute(
+                    "SELECT s.predictable,s.date,s.page,s.prediction,t.id,t.type, t.amount,t.player,t.player_id FROM crowdicate.predictions as s left join crowdicate.predictables as t on s.predictable = t.id WHERE STR_TO_DATE(s.date, '%m/%d/%Y') = CURDATE()"
+                )
+
+                rows = cursor.fetchall()
+
+            cnx.close()
+
+        results = pd.DataFrame(list(rows),
+                               columns=["predictable", "date", "page", "prediction", 'id', 'type', 'amount', 'player',
+                                        'player_id'])
+
+        predictions_live = filtered_df.merge(results[["player", "amount", "prediction", "page"]], how='left',
+                                             left_on=['player_name', 'outcome_point'], right_on=['player', "amount"])
+        predictions_live['diff'] = (((predictions_live['prediction'] - predictions_live['Im_Prob']) / predictions_live[
+            'Im_Prob']) * 100).round(1)
+
+        predictions_live = predictions_live.dropna(subset=['prediction', 'page', 'diff'])
+
+        # Group the data
+        grouped = predictions_live.groupby(['player_name', 'bookmaker_title', 'outcome_price', 'outcome_point'])
+
+        # Initialize the list to hold the final JSON structure
+        bets = []
+
+        # Iterate through the groups and construct the JSON structure
+        for (player_name, bookmaker_title, outcome_price, outcome_point), group in grouped:
+            predictions = []
+            for _, row in group.iterrows():
+                predictions.append({
+                    "page": row['page'],
+                    "diff": str(row['diff'])
+                })
+            bet = {
+                "player_name": player_name,
+                "bookmaker_title": bookmaker_title,
+                "outcome_price": str(outcome_price),
+                "outcome_point": str(outcome_point),
+                "predictions": predictions
+            }
+            bets.append(bet)
+
+        # Create the final JSON structure
+        final_json = {
+            "bets": bets
+        }
+
+        return json.dumps(final_json, indent=2)
+    else:
+        url = "https://api.the-odds-api.com/v4/sports/baseball_mlb/odds"
+        api_key = "22a6282c9744177b06acb842d34a02cb"
+        # API endpoint and key
+        url = "https://api.the-odds-api.com/v4/sports/baseball_mlb/odds"
+        api_key = "22a6282c9744177b06acb842d34a02cb"
+        if books is not None:
+            params = {
+                'apiKey': api_key,
+                'regions': 'us,us2',
+                ##'markets': 'h2h,spreads',
+                'bookmakers': books,
+                'markets': 'h2h',
+                'oddsFormat': 'american',
+                'commenceTimeFrom': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'commenceTimeTo': (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime(
+                    '%Y-%m-%dT%H:%M:%SZ')
+            }
+        else:
+            params = {
+                'apiKey': api_key,
+                'regions': 'us,us2',
+                ##'markets': 'h2h,spreads',
+                'markets': 'h2h',
+                'oddsFormat': 'american',
+                'commenceTimeFrom': datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
+                'commenceTimeTo': (datetime.utcnow() + timedelta(days=1)).replace(hour=6, minute=0).strftime(
+                    '%Y-%m-%dT%H:%M:%SZ')
+            }
+
         # Fetch the data from the API
         response = requests.get(url, params=params)
         data = response.json()
 
         # Extract and flatten markets data
+        flattened_markets = []
 
         for entry in data:
             game_info = {
-                "id": data["id"],
-                "sport_key": data["sport_key"],
-                "sport_title": data["sport_title"],
-                "commence_time": data["commence_time"],
-                "home_team": data["home_team"],
-                "away_team": data["away_team"]
+                "id": entry["id"],
+                "sport_key": entry["sport_key"],
+                "sport_title": entry["sport_title"],
+                "commence_time": entry["commence_time"],
+                "home_team": entry["home_team"],
+                "away_team": entry["away_team"]
             }
-            for bookmaker in data["bookmakers"]:
+            for bookmaker in entry["bookmakers"]:
                 bookmaker_info = {
                     "bookmaker_key": bookmaker["key"],
                     "bookmaker_title": bookmaker["title"]
@@ -1277,83 +1431,85 @@ def test_bet():
                         outcome_info = market_info.copy()
                         if "point" in outcome:
                             outcome_info.update({
-                                "player_name": outcome["description"],
                                 "outcome_name": outcome["name"],
                                 "outcome_price": outcome["price"],
                                 "outcome_point": outcome["point"]
                             })
                         else:
                             outcome_info.update({
-                                "player_name": outcome["description"],
                                 "outcome_name": outcome["name"],
                                 "outcome_price": outcome["price"],
                                 "outcome_point": 0
                             })
                         flattened_markets.append(outcome_info)
 
-    # Convert flattened markets data into a DataFrame
-    df = pd.DataFrame(flattened_markets)
-    max_indices = df.groupby(['outcome_name', 'outcome_point', 'player_name'])['outcome_price'].idxmax()
-    filtered_df = df.loc[max_indices]
-    filtered_df['Im_Prob'] = np.where(filtered_df['outcome_price'] >= 0, 100 / (100 + filtered_df['outcome_price']),
-                                      -filtered_df['outcome_price'] / (-filtered_df['outcome_price'] + 100))
+        # Convert flattened markets data into a DataFrame
+        df = pd.DataFrame(flattened_markets)
 
-    filtered_df = filtered_df[filtered_df['outcome_name'] == 'Over']
+        max_indices = df.groupby(['outcome_name','outcome_point'])['outcome_price'].idxmax()
+        # min_indices = df.groupby(['outcome_name','outcome_point'])['outcome_price'].idxmin()
 
-    cnx = mysql.connector.connect(user='doadmin', password='AVNS_Lkaktbc2QgJkv-oDi60',
-                                  host='db-mysql-nyc3-89566-do-user-8045222-0.c.db.ondigitalocean.com',
-                                  port=25060,
-                                  database='crowdicate')
-    if cnx and cnx.is_connected():
-        with cnx.cursor() as cursor:
-            result = cursor.execute(
-                "SELECT s.predictable,s.date,s.page,s.prediction,t.id,t.type, t.amount,t.player,t.player_id FROM crowdicate.predictions as s left join crowdicate.predictables as t on s.predictable = t.id WHERE STR_TO_DATE(s.date, '%m/%d/%Y') = CURDATE()"
+        # Combine the indices and filter the DataFrame
+        # unique_indices = max_indices.append(min_indices).unique()
+        filtered_df = df.loc[max_indices]
+
+        df['Im_Prob'] = np.where(df['outcome_price'] >= 0, 100 / (100 + df['outcome_price']),
+                                 -df['outcome_price'] / (-df['outcome_price'] + 100))
+        cnx = mysql.connector.connect(user='doadmin', password='AVNS_Lkaktbc2QgJkv-oDi60',
+                                      host='db-mysql-nyc3-89566-do-user-8045222-0.c.db.ondigitalocean.com',
+                                      port=25060,
+                                      database='crowdicate')
+        if cnx and cnx.is_connected():
+            with cnx.cursor() as cursor:
+                result = cursor.execute(
+                    "SELECT s.predictable,s.date,s.page,s.prediction,t.id,t.type, t.amount,t.player,t.player_id FROM crowdicate.predictions as s left join crowdicate.predictables as t on s.predictable = t.id WHERE STR_TO_DATE(s.date, '%m/%d/%Y') = CURDATE()"
                 )
 
-            rows = cursor.fetchall()
+                rows = cursor.fetchall()
 
-        cnx.close()
+            cnx.close()
 
-    results = pd.DataFrame(list(rows),
-                           columns=["predictable", "date", "page", "prediction", 'id', 'type', 'amount', 'player',
-                                    'player_id'])
+        results = pd.DataFrame(list(rows),
+                               columns=["predictable", "date", "page", "prediction", 'id', 'type', 'amount', 'player',
+                                        'player_id'])
+        predictions_live["outcome_point"] = ""
 
-    predictions_live = filtered_df.merge(results[["player", "amount", "prediction", "page"]], how='left',
-                                         left_on=['player_name', 'outcome_point'], right_on=['player', "amount"])
-    predictions_live['diff'] = (((predictions_live['prediction'] - predictions_live['Im_Prob']) / predictions_live[
-        'Im_Prob']) * 100).round(1)
+        predictions_live = filtered_df.merge(results[["player", "amount", "prediction", "page"]], how='left',
+                                             left_on=['outcome_name'], right_on=['player'])
+        predictions_live['diff'] = (((predictions_live['prediction'] - predictions_live['Im_Prob']) / predictions_live[
+            'Im_Prob']) * 100).round(1)
 
-    predictions_live = predictions_live.dropna(subset=['prediction', 'page', 'diff'])
+        predictions_live = predictions_live.dropna(subset=['prediction', 'page', 'diff'])
 
-    # Group the data
-    grouped = predictions_live.groupby(['player_name', 'bookmaker_title', 'outcome_price', 'outcome_point'])
+        # Group the data
+        grouped = predictions_live.groupby(['outcome_name', 'bookmaker_title', 'outcome_price', 'outcome_point'])
 
-    # Initialize the list to hold the final JSON structure
-    bets = []
+        # Initialize the list to hold the final JSON structure
+        bets = []
 
-    # Iterate through the groups and construct the JSON structure
-    for (player_name, bookmaker_title, outcome_price, outcome_point), group in grouped:
-        predictions = []
-        for _, row in group.iterrows():
-            predictions.append({
-                "page": row['page'],
-                "diff": str(row['diff'])
-            })
-        bet = {
-            "player_name": player_name,
-            "bookmaker_title": bookmaker_title,
-            "outcome_price": str(outcome_price),
-            "outcome_point": str(outcome_point),
-            "predictions": predictions
+        # Iterate through the groups and construct the JSON structure
+        for (outcome_name, bookmaker_title, outcome_price, outcome_point), group in grouped:
+            predictions = []
+            for _, row in group.iterrows():
+                predictions.append({
+                    "page": row['page'],
+                    "diff": str(row['diff'])
+                })
+            bet = {
+                "player_name": outcome_name,
+                "bookmaker_title": bookmaker_title,
+                "outcome_price": str(outcome_price),
+                "outcome_point": str(outcome_point),
+                "predictions": predictions
+            }
+            bets.append(bet)
+
+        # Create the final JSON structure
+        final_json = {
+            "bets": bets
         }
-        bets.append(bet)
 
-    # Create the final JSON structure
-    final_json = {
-        "bets": bets
-    }
-
-    return json.dumps(final_json, indent=2)
+        return json.dumps(final_json, indent=2)
 
 
 # Press the green button in the gutter to run the script.
