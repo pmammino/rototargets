693601f48eac3d9fd05ebdd3f0e9bab425527880
pmammino <paul.mammino@wagner.edu>
2024-08-27 12:08:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -183,15 +183,16 @@ def get_template(page_id):
                     splitting = test.split(' @ ')
                     link_list.append(splitting[0])
                     link_list.append(splitting[1])
-
-    current_week = 1
-    schedule = pd.read_csv("schedule.csv")
-    schedule["game"] = schedule["home_team"] + " @ " + schedule["away_team"]
-    schedule = schedule[schedule['week'] == current_week]
-    games = schedule["game"].tolist()
-    games = games + schedule["home_team"].tolist()
-    games = games + schedule["away_team"].tolist()
-    link_list = link_list + games
+                    
+    if any("NFL" in s for s in types):
+        current_week = 1
+        schedule = pd.read_csv("schedule.csv")
+        schedule["game"] = schedule["home_team"] + " @ " + schedule["away_team"]
+        schedule = schedule[schedule['week'] == current_week]
+        games = schedule["game"].tolist()
+        games = games + schedule["home_team"].tolist()
+        games = games + schedule["away_team"].tolist()
+        link_list = link_list + games
 
     cnx = mysql.connector.connect(user='doadmin', password='AVNS_Lkaktbc2QgJkv-oDi60',
                                   host='db-mysql-nyc3-89566-do-user-8045222-0.c.db.ondigitalocean.com',
